e4ee1a902b24aa1cdf6b90f84416e9bb47826311
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-02-05 16:13:01 +0100

    Beheer: fix links die missen (#106)
    Op dit moment faalt de link check omdat er enkele links
    zijn die niet bestaan. Hiermee fixen we de link checks door
    de correct URLs te gebruiken en de laatst gepubliceerde
    versie te negeren.

--- config.js
+++ config.js
@@ -4,6 +4,10 @@ const localConfig = {
   publishVersion: "0.8.0",
   github: "https://github.com/Logius-standaarden/logboek-dataverwerkingen",
 
+  // TODO: Voordat we releasen moet dit weg worden gehaald. Op dit moment is er nog
+  // geen publicatie van deze standaard.
+  latestVersion: "https://logius-standaarden.github.io/logboek-dataverwerkingen/",
+
   license: "cc-by",
   specStatus: "wv",
   specType: "st",
