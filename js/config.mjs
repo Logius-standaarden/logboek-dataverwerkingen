a84f98b8387aea06e0d9e7405bacdbd1cd52dcb3
Tim van der Lippe <Tim.Lippe@logius.nl>
2025-11-27 13:00:05 +0100

    Prepareer consultatieversie voor 1.0.0

--- js/config.mjs
+++ js/config.mjs
@@ -2,14 +2,20 @@ import { loadRespecWithConfiguration } from "https://logius-standaarden.github.i
 
 loadRespecWithConfiguration({
   maxTocLevel: 3,
-  specStatus: "WV",
+  specStatus: "CV",
   specType: "ST",
   pubDomain: "logboek",
   shortName: "dataverwerkingen",
-  publishDate: "2024-12-13",
-  publishVersion: "0.8.0",
+  publishDate: "2025-11-27",
+  publishVersion: "1.0.0",
   // TODO: verwijder voor publicatie
   latestVersion: "https://logius-standaarden.github.io/logboek-dataverwerkingen/",
+  sotdText: {
+    nl: {
+      sotd: "Status van dit document",
+      cv: `Dit is een consultatieversie. We moedigen gebruikers aan om meldingen of suggesties aan te maken via GitHub. Mocht dit niet mogelijk zijn, dan kunt u ook een e-mail sturen naar api@logius.nl`,
+    },
+  },
   prevVersion: [],
 
   edDraftURI: "https://logius-standaarden.github.io/logboek-dataverwerkingen/",
