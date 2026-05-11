60f0eb04b7fb3141c1191d123918fc9999833664
Nil Netra Barua <nil.barua@logius.nl>
2026-05-11 18:11:35 +0200

    Update href and add authors in config.mjs

--- js/config.mjs
+++ js/config.mjs
@@ -65,7 +65,8 @@ loadRespecWithConfiguration({
       title: "Medische informatica - Logging - Vastleggen van acties op persoonlijke gezondheidsinformatie"
     },
   "logboek juridisch beleidskader": {
-      href: "https://logius-standaarden.github.io/logboek-dataverwerkingen-juridisch-beleidskader/",
+      authors: ["Wouter Diephuis", "Mirian van Ansem"]
+      href: "https://gitdocumentatie.logius.nl/publicatie/logboek/juridisch/1.0.0/",
       publisher: "Logius",
       title: "Logboek Dataverwerkingen - Juridisch beleidskader"
     },
